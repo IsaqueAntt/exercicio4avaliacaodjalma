a9098bf3c45011a351dcc89f695130329f02dd67
isaqu <isaqu@RORONOA-ISAQUE>
2022-07-04 11:16:06 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio4avaliaçaodjalma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            {
+
+                for (int i = 1; i <= 50; i++)
+
+                {
+
+                    Console.WriteLine($"Number: {i}");
+
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
